a5ff958652e9917326ba47e33dce14f42c2c17c0
omrikiei <omrieival@gmail.com>
2020-01-29 23:46:37 +0200

    Brew formula update for ktunnel version v1.1.6

--- ktunnel.rb
+++ ktunnel.rb
@@ -2,16 +2,16 @@
 class Ktunnel < Formula
   desc "Network tunneling tool for kubernetes."
   homepage "https://github.com/omrikiei/ktunnel"
-  version "1.1.4"
+  version "1.1.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/omrikiei/ktunnel/releases/download/v1.1.4/ktunnel_1.1.4_Darwin_x86_64.tar.gz"
-    sha256 "4c770802a4d0c14b051e877a2d4f91ed5cdc83a6ea0a0e7b45f7fa34a1814bcc"
+    url "https://github.com/omrikiei/ktunnel/releases/download/v1.1.6/ktunnel_1.1.6_Darwin_x86_64.tar.gz"
+    sha256 "32ae824463ee59ff71dc1541e6aee574aab6d5fae6e76c36b19dc66b7d0a3af8"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/omrikiei/ktunnel/releases/download/v1.1.4/ktunnel_1.1.4_Linux_x86_64.tar.gz"
-      sha256 "da7e3cf49d396be46f598ea091c9afee47cb48be57a3cc6f4e083a64d55dbe4a"
+      url "https://github.com/omrikiei/ktunnel/releases/download/v1.1.6/ktunnel_1.1.6_Linux_x86_64.tar.gz"
+      sha256 "4d47cf37143ef9aa1b8152987102faa98f9b9cceb96fadf4c4f3ff4359acf235"
     end
   end
   
