da0a1756763dc8e699cc1f9512c20dac4c4337bf
Omri Eival <omrieival@gmail.com>
2024-12-27 16:45:56 +0200

    Update injector_test.go

--- pkg/k8s/injector_test.go
+++ pkg/k8s/injector_test.go
@@ -122,7 +122,60 @@ func TestGetPortForwardUrl(t *testing.T) {
 			},
 		},
 	}
-	
+
+	for _, table := range tables {
+		res := getPortForwardURL(&table.Config, table.Namespace, table.Pod)
+		if res.Scheme != table.Expected.Scheme || res.Host != table.Expected.Host || res.Path != table.Expected.Path {
+			t.Errorf("expected: %v, got: %v", table.Expected, res)
+		}
+	}
+}
+
+func TestGetPortForwardURL(t *testing.T) {
+	tables := []struct {
+		Config    rest.Config
+		Namespace string
+		Pod       string
+		Expected  *url.URL
+	}{
+		{
+			Config: rest.Config{
+				Host: "https://api.qa.kube.com",
+			},
+			Namespace: "default",
+			Pod:       "test",
+			Expected: &url.URL{
+				Scheme: "https",
+				Host:   "api.qa.kube.com",
+				Path:   "api/v1/namespaces/default/pods/test/portforward",
+			},
+		},
+		{
+			Config: rest.Config{
+				Host: "https://rancher.xyz.io/k8s/clusters/c-wfdqx",
+			},
+			Namespace: "default",
+			Pod:       "test",
+			Expected: &url.URL{
+				Scheme: "https",
+				Host:   "rancher.xyz.io",
+				Path:   "/k8s/clusters/c-wfdqx/api/v1/namespaces/default/pods/test/portforward",
+			},
+		},
+		{
+			Config: rest.Config{
+				Host: "https://srv01.mydomain.de:6443",
+			},
+			Pod:       "myapp-5b65c8777b-dd54r",
+			Namespace: "default",
+			Expected: &url.URL{
+				Scheme: "https",
+				Host:   "srv01.mydomain.de:6443",
+				Path:   "api/v1/namespaces/default/pods/myapp-5b65c8777b-dd54r/portforward",
+			},
+		},
+	}
+
 	for _, table := range tables {
 		res := getPortForwardURL(&table.Config, table.Namespace, table.Pod)
 		if res.Scheme != table.Expected.Scheme || res.Host != table.Expected.Host || res.Path != table.Expected.Path {
